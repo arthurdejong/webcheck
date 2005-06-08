bfe6848ae045b3bddfe6f29a7bcd4d9fae35df61
Arthur de Jong <arthur@arthurdejong.org>
2005-06-08 19:10:39 +0000

    claiming copyright on empty files is silly
    git-svn-id: http://arthurdejong.org/svn/webcheck/webcheck@34 86f53f14-5ff3-0310-afe5-9b438ce3f40c

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -1,19 +1,2 @@
 
 # __init__.py - empty file
-#
-# Copyright (C) 1998, 1999 Albert Hopkins (marduk) <marduk@python.net>
-# Copyright (C) 2002 Mike Meyer <mwm@mired.org>
-#
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
