1e972a3af486d1e7fb93eacc2c810fe1a30a0c75
Arthur de Jong <arthur@arthurdejong.org>
2005-07-24 08:55:02 +0000

    rename whatsold and whatsnew plugins to old and new
    git-svn-id: http://arthurdejong.org/svn/webcheck/webcheck@85 86f53f14-5ff3-0310-afe5-9b438ce3f40c

--- plugins/old.py
+++ plugins/old.py
@@ -1,5 +1,5 @@
 
-# whatsnew.py - plugin to list recently modified pages
+# old.py - plugin to list old pages
 #
 # Copyright (C) 1998, 1999 Albert Hopkins (marduk) <marduk@python.net>
 # Copyright (C) 2002 Mike W. Meyer <mwm@mired.org>
@@ -19,12 +19,12 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
 
-"""Present a list of recently modified pages."""
+"""Present a list of potentially outdated pages."""
 
-__title__ = "what's new"
+__title__ = "what's old"
 __author__ = 'Arthur de Jong'
 __version__ = '1.1'
-__description__ = 'These pages habe been recently modified.'
+__description__ = 'These pages habe been modified a long time ago and may be outdated.'
 
 import config
 import plugins
@@ -33,10 +33,10 @@ import time
 SECS_PER_DAY=60*60*24
 
 def generate(fp,site):
-    """Output the list of recently modified pages to the specified file descriptor."""
+    """Output the list of outdated pages to the specified file descriptor."""
     fp.write('   <ul>\n')
     links=site.linkMap.values()
-    links.sort(lambda a, b: cmp(b.mtime, a.mtime))
+    links.sort(lambda a, b: cmp(a.mtime, b.mtime))
     for link in links:
         if not link.ispage:
             continue
@@ -45,7 +45,7 @@ def generate(fp,site):
         if not link.isinternal:
             continue
         age = (time.time()-link.mtime)/SECS_PER_DAY
-        if (age is not None) and (age <= config.REPORT_WHATSNEW_URL_AGE):
+        if age and (age >= config.REPORT_WHATSOLD_URL_AGE):
             fp.write(
               '    <li>\n' \
               '     %(link)s\n' \
@@ -53,4 +53,6 @@ def generate(fp,site):
               '    </li>\n' \
               % { 'link':  plugins.make_link(link),
                   'age':   age })
+            # add link to problem database
+            plugins.add_problem('Old Link: %d days old' % age ,link)
     fp.write('   </ul>\n')
