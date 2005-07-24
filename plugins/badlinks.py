571339c65cf8390bafc0730ed266d03f1800c285
Arthur de Jong <arthur@arthurdejong.org>
2005-07-24 08:47:33 +0000

    remove HTTP status code handling from here as this should be done by the HTTP module
    git-svn-id: http://arthurdejong.org/svn/webcheck/webcheck@81 86f53f14-5ff3-0310-afe5-9b438ce3f40c

--- plugins/badlinks.py
+++ plugins/badlinks.py
@@ -31,44 +31,6 @@ import config
 import plugins
 import xml.sax.saxutils
 
-HTTP_STATUS_CODES = {'100':"Continue",
-                     '101':"Switching Protocols",
-                     '200':"OK",
-                     '201':"Created",
-                     '202':"Accepted",
-                     '204':"No Content",
-                     '205':"Reset Content",
-                     '206':"Partial Content",
-                     '300':"Multiple Choices",
-                     '301':"Moved Permanently",
-                     '302':"Moved Temporarily",
-                     '303':"See Other",
-                     '304':"Not Modified",
-                     '305':"Use Proxy",
-                     '400':"Bad Request",
-                     '401':"Unauthorized",
-                     '402':"Payment Required",
-                     '403':"Forbidden",
-                     '404':"Not Found",
-                     '405':"Method Not Allowed",
-                     '406':"Not Acceptable",
-                     '407':"Proxy Authentication Required",
-                     '408':"Request Time-out",
-                     '409':"Conflict",
-                     '410':"Gone",
-                     '411':"Length Required",
-                     '412':"Precondition Failed",
-                     '413':"Request Entity Too Large",
-                     '414':"Request-URI Too Large",
-                     '415':"Unsupported Media Type",
-                     '500':"Internal Server Error",
-                     '501':"Not Implemented",
-                     '502':"Bad Gateway",
-                     '503':"Service Unavailable",
-                     '504':"Gateway Time-out",
-                     '505':"HTTP Version not supported"
-                     }
-
 def generate(fp,site):
     """Present the list of bad links to the given file descriptor."""
     fp.write('   <ol>\n')
@@ -78,8 +40,6 @@ def generate(fp,site):
         if link.status is None:
             continue
         status = str(link.status)
-        if status in HTTP_STATUS_CODES.keys():
-            status = status + "=" + HTTP_STATUS_CODES[status]
         fp.write(
           '    <li>\n' \
           '     %(badurl)s\n' \
