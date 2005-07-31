16e714c407be1ca8b45e3e9c5d1f982fde04cfc9
Arthur de Jong <arthur@arthurdejong.org>
2005-07-31 20:58:20 +0000

    get files ready for 1.9.2 release
    git-svn-id: http://arthurdejong.org/svn/webcheck/webcheck@120 86f53f14-5ff3-0310-afe5-9b438ce3f40c

--- config.py
+++ config.py
@@ -27,7 +27,7 @@ items should be changeble from the command line."""
 import urllib
 
 # Current version of webcheck.
-VERSION = "1.9.1"
+VERSION = "1.9.2"
 
 # The homepage of webcheck.
 HOMEPAGE = "http://ch.tudelft.nl/~arthur/webcheck/"
