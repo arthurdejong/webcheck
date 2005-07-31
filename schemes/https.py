9bcbf3ec40adf0a4ce121bc134ccee48993dde11
Arthur de Jong <arthur@arthurdejong.org>
2005-07-31 09:14:21 +0000

    add https module as a wrapper to the http module
    git-svn-id: http://arthurdejong.org/svn/webcheck/webcheck@115 86f53f14-5ff3-0310-afe5-9b438ce3f40c

---
+++ schemes/https.py
@@ -0,0 +1,26 @@
+
+# https.py - handle urls with a https scheme
+#
+# Copyright (C) 2005 Arthur de Jong
+# 
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+
+"""This module defines the functions needed for filling in information in Link
+objects for urls using the https scheme."""
+
+import schemes.http
+
+# just direct us to the http module that can fetch https links
+fetch = schemes.http.fetch
