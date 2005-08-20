70c0f8bdfb3f46e31fd8e9feabe5b28bb213926d
Arthur de Jong <arthur@arthurdejong.org>
2005-08-20 16:30:45 +0000

    first attempt at a very simple CSS parser that just summarises links to images and imported CSS files
    git-svn-id: http://arthurdejong.org/svn/webcheck/webcheck@146 86f53f14-5ff3-0310-afe5-9b438ce3f40c

--- parsers/css.py
+++ parsers/css.py
@@ -17,4 +17,31 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
 
-mimetypes = ()
+mimetypes = ('text/css',)
+
+import urlparse
+import re
+
+# pattern for matching /* ... */ comments in css
+_commentpattern = re.compile('/\*.*?\*/', re.IGNORECASE|re.DOTALL)
+
+# pattern for matching @import "url" statments in css
+_importpattern = re.compile('@import\s+["\']([^"\']*)["\']',re.IGNORECASE|re.DOTALL)
+
+# pattern for matching url(...) in css
+_urlpattern = re.compile('url\(["\']?(.*?)["\']?\)')
+
+def parse(content, link):
+    """Parse the specified content and extract information for crawling the
+    site further."""
+    print "PARSE CSS"
+    # strip out comments from the content
+    content = _commentpattern.sub('', content)
+    # handler @imports
+    for i in _importpattern.findall(content):
+        print "IMPORT="+i
+        link.add_embed(urlparse.urljoin(link.url, i))
+    # handle url()s
+    for i in _urlpattern.findall(content):
+        print "URL="+i
+        link.add_embed(urlparse.urljoin(link.url, i))
