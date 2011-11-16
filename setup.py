3b9246292844ca6b5d338fe900a3600054602067
Devin Bayer <l@t-0.be>
2011-11-16 11:17:29 +0000

    add setup.py
    git-svn-id: http://arthurdejong.org/svn/webcheck/webcheck@462 86f53f14-5ff3-0310-afe5-9b438ce3f40c

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+setup(
+    name = 'webcheck',
+    version = '1.10.5a',
+    packages = find_packages(),
+    scripts = ['cmd.py'],
+    install_requires = ['distribute', 'sqlalchemy'],
+    extras_require = {
+        'tidy': ['utidylib'],
+        'soup': ['beautifulsoup'],
+    },
+    
+    # metadata for pypi
+    author = 'Arthur de Jong',
+    author_email = 'arthur@arthurdejong.org',
+    description = 'webcheck is a website checking tool for webmasters',
+    license = 'GPL',
+    keywords = 'spider',
+    url = 'http://arthurdejong.org/webcheck/',
+)
